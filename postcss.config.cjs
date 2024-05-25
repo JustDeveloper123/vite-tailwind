31637d7f2052007b5fcdcae0503d82e8953167f1
Zhernakov Oleksandr <oleksandr.portfolio@gmail.com>
2024-05-25 18:18:36 +0300

    latest version 25.05.2024

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,10 @@
-// module.exports = ({ env }) => ({ plugins: [ require('autoprefixer')() ] })
-
 module.exports = {
   plugins: [
     require('postcss-sort-media-queries')({
-      sort: 'mobile-first', // default value
+      sort: 'desktop-first', // sort: mobile-first | desktop-first | custom function
     }),
+    require('autoprefixer'), // підтримка стилів визначається в package.json ключем "browserslist", найбільш поширене значення "cover 99.5%"
+    // Також, autoprefixer сам по собі може бути підключений в конфігу Vite не прописуючи його в цьому файлі. В інтернеті є інформація
     require('tailwindcss'),
-    require('autoprefixer'),
   ],
 };
